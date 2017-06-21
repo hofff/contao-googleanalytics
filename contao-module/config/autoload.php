85fb22dd3683011853bc70e2b33ebb136f7eeb2e
cboelter <christopher@boelter.eu>
2017-06-21 09:08:47 +0200

    contao 3.5 compat, added composer support, refactoring google analytics

---
+++ contao-module/config/autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * @package Googleanalytics
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(
+    array
+    (
+        'ce_download_ga'      => 'system/modules/googleanalytics/templates',
+        'ce_downloads_ga'     => 'system/modules/googleanalytics/templates',
+        'mod_googleanalytics' => 'system/modules/googleanalytics/templates',
+    )
+);
---
+++ contao-module/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * PHP version 5
+ *
+ * @copyright  Jan Theofel 2011, ETES GmbH 2010
+ * @author     Jan Theofel <jan@theofelde>
+ * @package    googleanalytics
+ * @license    LGPL
+ * @filesource
+ */
+
+// register parse template hook
+$GLOBALS['TL_HOOKS']['parseFrontendTemplate'][] =
+    array('Hofff\\Contao\\Frontend\\GoogleAnalytics', 'addGoogleAnalytics');
+
+// register custom inserttags
+$GLOBALS['TL_HOOKS']['replaceInsertTags'][] =
+    array('Hofff\\Contao\\Frontend\\GoogleAnalytics', 'gaInsertTag');
